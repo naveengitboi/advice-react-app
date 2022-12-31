89d4c0f80fc41171ffd48794d86af9a0eb783399
naveengitboi <naveenjangiti9@gmail.com>
2023-01-01 01:38:13 +0530

    Done responsive web design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import React from 'react'
-function App() {
-    return ( 
-        <h1>Hello</h1>
-     );
+import axios from 'axios'
+import './App.css'
+
+class App extends React.Component{
+    
+    state = {advice: ''}
+
+    componentDidMount(){
+        this.fetchAdvice()
+    }
+
+    fetchAdvice = () => {
+        axios.get("https://api.adviceslip.com/advice")
+            .then((response) => {
+                const {advice} = response.data.slip
+                
+                this.setState({advice:advice})
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+    
+
+    render(){
+        return(
+            <div className='container'>
+                <div className="card">
+                    <h1>{this.state.advice}</h1>
+                    <button onClick={this.fetchAdvice}>Load Advice</button>
+                </div>
+            </div>
+        )
+    }
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ReactDOM } from 'react'
+import ReactDOM  from 'react-dom'
 import App from './App'
 
-ReactDOM.rendar(<App/>, document.getElementById('root'))
+ReactDOM.render(<App/>, document.getElementById('root'))
